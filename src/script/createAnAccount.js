18b57c7fb26b248abc83abccfdf0ff278f176102
unknown <carlosvieira0712@digitalcollege.internal>
2025-11-18 15:30:02 -0300

    Finish login call page register

--- src/script/createAnAccount.js
+++ src/script/createAnAccount.js
@@ -3,8 +3,26 @@ const username = document.querySelector("#username-create");
 const password = document.querySelector("#password-create");
 
 const createBtn = document.querySelector("#create-account-btn");
-createBtn.addEventListener("click", () => {
-  alert("Account created successfully! (simulation)");
+
+createBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const emailValue = email.value;
+  const usernameValue = username.value;
+  const passwordValue = password.value;
+
+  if (emailValue === "" || usernameValue === "" || passwordValue === "") {
+    alert("Fill in all fields!");
+    return;
+  }
+
+  sessionStorage.setItem("email", emailValue);
+  sessionStorage.setItem("username", usernameValue);
+  sessionStorage.setItem("password", passwordValue);
+
+  alert("Registration successful.");
+
+  window.location.href = "login.html";
 });
 
 const backToLogin = document.querySelector("#go-to-login");
